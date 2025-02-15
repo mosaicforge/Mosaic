934be9e01d9c2b60dbb80de467a33a0dbcaa0b57
Christophe <cvauclair@protonmail.com>
2025-02-15 17:24:38 -0500

    misc: Remove old code

--- sdk/src/mapping_old/entity_queries/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod find_many;
-pub mod type_filter;
-
-pub use find_many::FindMany;
-pub use type_filter::TypeFilter;
--- sdk/src/mapping_old/query_utils/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod attributes_filter;
-pub mod list_filter;
-pub mod order_by;
-pub mod query_part;
-pub mod scalar_filter;
