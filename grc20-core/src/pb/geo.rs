5f66d0eb5c72145c679d4c7ee9f7df4ab7023cc7
Christophe <cvauclair@protonmail.com>
2025-04-04 11:48:42 -0400

    style: fix comments

--- grc20-core/src/pb/geo.rs
+++ grc20-core/src/pb/geo.rs
@@ -99,9 +99,9 @@ pub struct GeoPersonalSpaceAdminPluginsCreated {
 /// An editor has editing and voting permissions in a DAO-based space. Editors join a space
 /// one of two ways:
 /// 1. They submit a request to join the space as an editor which goes to a vote. The editors
-///     in the space vote on whether to accept the new editor.
+/// in the space vote on whether to accept the new editor.
 /// 2. They are added as a set of initial editors when first creating the space. This allows
-///     space deployers to bootstrap a set of editors on space creation.
+/// space deployers to bootstrap a set of editors on space creation.
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct InitialEditorAdded {
