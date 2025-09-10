5f718b865f99d08a58c019fce44c1ac2ee8c8c16
0xtuareg <saunaflor9@gmail.com>
2025-09-10 18:00:45 -0500

    fix(sdk): Id generation + space import (#23)

---
+++ models/block.rs
@@ -0,0 +1,10 @@
+Feat: Refactor node stack to expose GraphQL API
+* feat: Initial prototype for GraphQL server
+
+* feat: Add GraphQL API prototype
+
+* refactor: Rename `node` to `indexer`
+
+* devops: Add `api` to docker compose
+
+* misc: Remove old files
